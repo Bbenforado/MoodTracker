1b329a65d090c0e40baef82c29e28a3578988bba
Blanche <kittyr@hotmail.fr>
2019-01-03 19:15:41 +0100

    implemented the onStop() method in the main activity

--- app/src/main/java/com/example/blanche/moodtracker/controller/MainActivity.java
+++ app/src/main/java/com/example/blanche/moodtracker/controller/MainActivity.java
@@ -14,12 +14,22 @@ import com.example.blanche.moodtracker.R;
 import com.example.blanche.moodtracker.adapters.PageAdapter;
 import com.example.blanche.moodtracker.controller.VerticalViewPager;
 
+import java.util.Calendar;
+import java.util.Date;
+
+import static android.text.format.DateUtils.DAY_IN_MILLIS;
+
 public class MainActivity extends AppCompatActivity {
 
     VerticalViewPager verticalViewPager;
     private SharedPreferences preferences;
     private int currentFragment;
     public static final String KEY_CURRENT_FRAGMENT = "currentFragment";
+    public static final String KEY_TIME = "time";
+    public static final String KEY_DAY_ONE = "dayOne";
+
+    int i = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +40,10 @@ public class MainActivity extends AppCompatActivity {
 
 
         this.configureVerticalViewPager();
+
+
+        //si les preference avec l humeur selectionee sont remplies alors on set le current item à la derniere humeur selectionee
+
         //set the default fragment to the happy smiley
         verticalViewPager.setCurrentItem(1);
     }
@@ -44,30 +58,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    //private void setTheWeeksMood() {
-    // for (int i = 1; i <= 7; i++) {
-
-
-    //  }
 
     @Override
     protected void onStop() {
         super.onStop();
         //get the last fragment selected
         currentFragment = verticalViewPager.getCurrentItem();
-        System.out.println("currentfragment is " + currentFragment);
 
-        //put in it the preferences
-        preferences.edit().putInt(KEY_CURRENT_FRAGMENT, currentFragment).apply();
+        Calendar calendar = Calendar.getInstance();
 
-        System.out.println(" preferences" + preferences.getInt(KEY_CURRENT_FRAGMENT, 0));
-
+        //si le temps écoulé entre le current time et le dernier temps enregistré est plus grand que le temps d'une journée en mllis
+        if (calendar.getTimeInMillis() - preferences.getLong(KEY_TIME, 0) > DAY_IN_MILLIS) {
+            //alors crée une nouvelle préférence qui enregistre la derniere humeur
+            preferences.edit().putInt("day" + i, currentFragment).apply();
+            //dans key time on enregistre le dernier temps
+            preferences.edit().putLong(KEY_TIME, calendar.getTimeInMillis()).apply();
+            i++;
+        } else {
+            preferences.edit().putInt(KEY_DAY_ONE, currentFragment).apply();
+            preferences.edit().putLong(KEY_TIME, calendar.getTimeInMillis()).apply();
+        }
+        System.out.println("i = " + i);
+        System.out.println("day one = " + preferences.getInt("day 1", 0));
+        System.out.println("day two = " + preferences.getInt("day 2", 0));
+        System.out.println("day three = " + preferences.getInt("day 3", 0));
+        System.out.println("DAY_ONE = " + preferences.getInt(KEY_DAY_ONE, 0));
 
     }
-
-    //faire une boucle qui fait 7 tours et pour chaque tour
-    //chaque tour doit faire 24h
-    //à chaque tour on crée une nouvelle preference DAY_1 etc et on met la valeur de key current fragment dans une autre shared preference day1
 
 
 }
